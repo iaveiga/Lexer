c6862632c6a186afc36c121920065676e059f104
Iván Aveiga <ijaveiga@gmail.com>
2013-08-22 00:23:54 -0500

    Leemos archivos correctamente

---
+++ Main.hs
@@ -0,0 +1,9 @@
+import System.IO
+import Data.List.Split
+--Dependencies splitOn
+
+main = do
+	handle <- openFile "codigo.c" ReadMode
+	contents <- hGetContents handle
+	putStr contents
+	hClose handle
