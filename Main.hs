adcbcddffb106450f68f729957c21eda9bd91d26
Iván Aveiga <ijaveiga@gmail.com>
2013-09-01 19:29:32 -0500

    Asigna un token dado un lexeme
    Por ahora solo funciona con los tokens de operator y de reserved words,
    aún no reconoce funciones como printf, scanf, y demás, aún no sé como
    hacer eso.

--- Main.hs
+++ Main.hs
@@ -1,11 +1,25 @@
 import System.IO
 import Data.List.Split
---Dependencies splitOn
+import Data.Char
+import Text.Regex.Posix
 
 main = do
-	handle <- openFile "codigo.c" ReadMode
-	contents <- hGetContents handle
-	putStr contents
-	hClose handle
-	cars<-splitOn contents " "
-	putStr cars
+	putStr "S"
+	
+asignar :: String -> String	
+asignar lexeme 
+	| en_lista lexeme ["+","-","*","/","%","^"] = "OPERATOR"
+	| en_lista lexeme ["auto", "break", "case", "const", "continue", "default", "do", "else", "enum" ] = "RESERVED_WORD"
+	| en_lista lexeme ["extern", "for", "goto", "if", "register", "return", "signed", "sizeof"] = "RESERVED_WORD"
+	| en_lista lexeme ["static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"] = "RESERVED_WORD"
+	| en_lista lexeme ["int", "float", "char", "double", "long"] = "PRIMITIVE_TYPE"
+	| otherwise = "ERROR"
+
+--Si un elemento (String) está en una lista de String.		
+en_lista :: String -> [String] -> Bool
+en_lista _ [] = False
+en_lista lexeme (x:xs) = do
+	if map toLower lexeme == x then
+		True
+	else
+		en_lista lexeme xs
